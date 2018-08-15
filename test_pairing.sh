fc44ace54a124e7e9922e1cbcc2f4aa2c8bf6d4b
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-15 16:06:51 -0700

    comment test file

--- test_pairing.sh
+++ test_pairing.sh
@@ -1,20 +1,40 @@
 NOTEBOOK_IMAGE=$(python -c "import yaml; f = open('jupyter-config.yml'); spec = yaml.load(f.read()); print('{}:{}'.format(spec['jupyterhub']['singleuser']['image']['name'], spec['jupyterhub']['singleuser']['image']['tag']));")
+
+echo "pull notebook $NOTEBOOK_IMAGE"
 docker pull $NOTEBOOK_IMAGE
-docker run -d --name notebook -t $NOTEBOOK_IMAGE
+
+echo "start notebook image"
+docker start -d --name notebook -t $NOTEBOOK_IMAGE /bin/bash
+
+echo "copy worker template from running notebook"
 docker cp notebook:/pre-home/worker-template.yml worker-template.yml
+
 WORKER_IMAGE=$(python -c "import yaml; f = open('worker-template.yml'); spec = yaml.load(f.read()); print(spec['spec']['containers'][0]['image']);")
+echo "retrieved worker image $WORKER_IMAGE from notebook worker-template"
+
+echo "shut down notebook"
 docker stop notebook
 docker rm notebook
+
+echo "pull worker $WORKER_IMAGE"
 docker pull $WORKER_IMAGE
-# start scheduler in notebook server
+
+echo "start scheduler in notebook server"
 docker run -p 127.0.0.1:8888:8888 -p 127.0.0.1:8786:8786 -p 127.0.0.1:8787:8787 -d $NOTEBOOK_IMAGE start.sh /opt/conda/bin/dask-scheduler --port 8786 --bokeh-port 8787 &
-# start worker
+
+echo "start worker"
 docker run -p 127.0.0.1:8666:8666 -p 127.0.0.1:8785:8785 -d $WORKER_IMAGE /opt/conda/bin/dask-worker 127.0.0.1:8786 --worker-port 8666 --nanny-port 8785 &
-# notebook server for user connection
+
+echo "notebook server for user connection"
 docker create --name tester -p 127.0.0.1:8765:8765 $NOTEBOOK_IMAGE
-# copy test suite to test image
+
+echo "copy test suite to test image"
 docker cp notebook_test.py tester:/usr/bin
-# start the tester notebook
+
+echo "start the tester notebook"
 docker start tester
-# run test suite
+
+echo "run test suite"
 docker exec tester python /usr/bin/notebook_test.py
+
+echo "done"
