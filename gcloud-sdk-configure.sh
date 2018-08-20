d725e4d88bcd1a6560463e1403d999c3e40acca2
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-20 16:09:18 -0700

    add kubectl again :(

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-# gcloud components update --quiet
+gcloud components update --quiet
+gcloud components install kubectl --quiet
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
