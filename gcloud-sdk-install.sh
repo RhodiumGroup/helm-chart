4a62d22d94497e916cc86fd494c576345895a467
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-20 16:15:28 -0700

    install google cloud from tarball

--- gcloud-sdk-install.sh
+++ gcloud-sdk-install.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-curl https://sdk.cloud.google.com/ > install_google_cloud.sh
-bash install_google_cloud.sh --disable-prompts
+curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-212.0.0-linux-x86_64.tar.gz > google-cloud-sdk-212.0.0-linux-x86_64.tar.gz
+tar zxvf google-cloud-sdk-212.0.0-linux-x86_64.tar.gz google-coud-sdk
+bash google-cloud-sdk/install.sh --disable-prompts
