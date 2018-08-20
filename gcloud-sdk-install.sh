2742fceb85be6f314ae4fcfc6fe9daf35cdb440a
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-20 16:27:26 -0700

    quiet, install kubctl at installation

--- gcloud-sdk-install.sh
+++ gcloud-sdk-install.sh
@@ -4,4 +4,4 @@ set -e
 
 curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-212.0.0-linux-x86_64.tar.gz > google-cloud-sdk-212.0.0-linux-x86_64.tar.gz
 tar zxvf google-cloud-sdk-212.0.0-linux-x86_64.tar.gz google-cloud-sdk
-bash google-cloud-sdk/install.sh --disable-prompts
+bash google-cloud-sdk/install.sh --quiet --additional-components kubectl
