87c9673008c393d7236696cf50f9ab39c9eed414
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-21 01:26:58 -0700

    comment out all the stuff

--- deploy-test.sh
+++ deploy-test.sh
@@ -9,22 +9,22 @@ bash gcloud-sdk-configure.sh
 echo "get credentials"
 gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
 
-echo "create clusterrolebinding cluster-admin-binding"
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
+# echo "create clusterrolebinding cluster-admin-binding"
+# kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
 
-echo "create tiller"
-kubectl --namespace kube-system create sa tiller
+# echo "create tiller"
+# kubectl --namespace kube-system create sa tiller
 
-echo "create clusterrolebinding tiller"
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+# echo "create clusterrolebinding tiller"
+# kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 
-echo "init"
-helm init --client-only --service-account tiller
+# echo "init"
+# helm init --client-only --service-account tiller
 
-echo "patch deployment"
-kubectl --namespace=kube-system patch deployment tiller-deploy \
-    --type=json \
-    --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+# echo "patch deployment"
+# kubectl --namespace=kube-system patch deployment tiller-deploy \
+#     --type=json \
+#     --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 
 echo "repo add jupyterhub"
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
@@ -38,7 +38,7 @@ helm dependency update rhg-hub
 echo "repo update"
 helm repo update
 
-helm upgrade --install --dry-run test-hub rhg-hub -f jupyter-config.yml \
+helm upgrade --dry-run test-hub rhg-hub -f jupyter-config.yml \
     --set jupyterhub.cull.enabled=true \
     --set jupyterhub.proxy.service.loadBalancerIP=$LOAD_BALANCER_IP_TEST \
     --set jupyterhub.proxy.https.hosts={$DOMAIN_TEST} \
