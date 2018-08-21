8c0551f518b7b846661ebafa0586ea9463a0bbf8
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-20 18:38:57 -0700

    try adding tiller

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -7,3 +7,6 @@ set -e
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
+
+helm init --service-account tiller
+helm dependency update rhg-hub
