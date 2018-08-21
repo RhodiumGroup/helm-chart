38e8864a45a2d0375229ae1a36880e080df10f4b
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-21 00:41:49 -0700

    echoo

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -8,17 +8,32 @@ gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIAL
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
 
+echo "create clusterrolebinding cluster-admin-binding"
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
+
+echo "create tiller"
 kubectl --namespace kube-system create sa tiller
+
+echo "create clusterrolebinding tiller"
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 
 # export HELM_HOST=localhost:44134
+echo "init"
 helm init --client-only --service-account tiller
+
+echo "patch deployment"
 kubectl --namespace=kube-system patch deployment tiller-deploy \
     --type=json \
     --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 
+echo "repo add jupyterhub"
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+
+echo "repo add rhg-hub"
 helm repo add rhg-hub https://rhodiumgroup.github.io/helm-chart/
+
+echo "dependency update rhg-hub"
 helm dependency update rhg-hub
+
+echo "repo update"
 helm repo update
