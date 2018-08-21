ace772b2fd72e519f2a7d01e1bb57c972a987252
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-21 00:02:15 -0700

    create service account

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -8,7 +8,16 @@ gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIAL
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
 
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
+kubectl --namespace kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+
+export HELM_HOST=localhost:44134
 helm init --client-only --service-account tiller
+kubectl --namespace=kube-system patch deployment tiller-deploy \
+    --type=json \
+    --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo add rhg-hub https://rhodiumgroup.github.io/helm-chart/
 helm dependency update rhg-hub
