60c785ce3ddcf5732161cc75db35f12fec412c4d
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-20 22:12:18 -0700

    try not client only

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -8,7 +8,7 @@ gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIAL
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
 
-helm init --client-only --service-account tiller
+helm init --service-account tiller
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo add rhg-hub https://rhodiumgroup.github.io/helm-chart/
 helm dependency update rhg-hub
