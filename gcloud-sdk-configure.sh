1808d772e98c21cf68788c115463b29cfdca6760
Michael Delgado <delgado.michaelt@gmail.com>
2018-08-21 01:21:43 -0700

    move around creds

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -4,6 +4,12 @@ set -e
 
 # gcloud components update --quiet
 # gcloud components install kubectl --quiet
+
+echo "activate-service-account"
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+
+echo "config set project"
 gcloud config set project $PROJECT_ID
+
+echo "config set compute/zones"
 gcloud config set compute/zone $zone
