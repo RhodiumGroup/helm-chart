3fb0c2c38c42989a893461a5c21cd4ce9dcbbb87
Michael Delgado <delgado.michaelt@gmail.com>
2020-04-02 16:16:17 -0700

    update impactlab to helm3 & new pangeo-style cluster

--- deploy/create_scripts/impactlab.sh
+++ deploy/create_scripts/impactlab.sh
@@ -2,43 +2,80 @@
 
 set -e
 
-# Make sure you're logged in to gcloud and have the correct permissions
 EMAIL=$(gcloud config get-value account)
-PROJECTID=$(gcloud config get-value project)
-ZONE=$(gcloud config get-value compute/zone)
-CLUSTER_NAME=test-hub
-NUM_NODES=1 #adjust this to the size of cluster you want
-MAX_WORKER_NODES=100
-MIN_WORKER_NODES=0
+PROJECTID=compute-impactlab
+REGION=us-west
+ZONE=us-west1-a
+CLUSTER_NAME=impactlab-hub
+DEPLOYMENT_NAME=impactlab-hub
+URL=compute.impactlab.org
+HELM_SPEC=impactlab-config.yml
+NUM_NODES=1
+MAX_JUPYTER_NODES=500
+MIN_JUPYTER_NODES=0
+MAX_DASK_NODES=5000
+MIN_DASK_NODES=0
+DISK_SIZE=100
+NB_MACHINE_TYPE=n1-highmem-8
+WORKER_MACHINE_TYPE=n1-highmem-8
+PREEMPTIBLE_FLAG=--preemptible
+
 
 # Start cluster on Google cloud
 gcloud container clusters create $CLUSTER_NAME --num-nodes=$NUM_NODES --machine-type=n1-standard-2 --zone=$ZONE --project=$PROJECTID
 
-#this enables autoscaling of the cluster
-gcloud container node-pools create worker-pool --zone=$ZONE --cluster=$CLUSTER_NAME --machine-type=n1-standard-8 --preemptible --enable-autoscaling --num-nodes=$MIN_WORKER_NODES --max-nodes=$MAX_WORKER_NODES --min-nodes=$MIN_WORKER_NODES --disk-size=50
+# get rid of default pool that we don't want
+echo; echo deleting default pool
+gcloud container node-pools delete default-pool --cluster $CLUSTER_NAME \
+  --zone=$ZONE --project=$PROJECTID --quiet
+
+# core-pool
+echo; echo creating core pool...
+core_machine_type="n1-standard-2"
+core_labels="hub.jupyter.org/node-purpose=core"
+gcloud container node-pools create core-pool --cluster=${CLUSTER_NAME} \
+   --machine-type=${core_machine_type} --zone=${ZONE} --num-nodes=2 \
+   --node-labels ${core_labels}
+
+# jupyter-pools
+echo; echo creating jupyter pool...
+jupyter_taints="hub.jupyter.org_dedicated=user:NoSchedule"
+jupyter_labels="hub.jupyter.org/node-purpose=user"
+gcloud container node-pools create jupyter-pool --cluster=${CLUSTER_NAME} \
+    --machine-type=${NB_MACHINE_TYPE} --disk-type=pd-ssd --zone=${ZONE} \
+    --num-nodes=0 --enable-autoscaling --min-nodes=0 \
+    --max-nodes=${MAX_JUPYTER_NODES} --node-taints ${jupyter_taints} \
+    --node-labels ${jupyter_labels}
+
+# dask-pool
+echo; echo creating dask pool...
+dask_taints="k8s.dask.org_dedicated=worker:NoSchedule"
+dask_labels="k8s.dask.org/node-purpose=worker"
+gcloud container node-pools create dask-pool --cluster=${CLUSTER_NAME} \
+    --machine-type=${WORKER_MACHINE_TYPE} --disk-type=pd-ssd --zone=${ZONE} \
+    --num-nodes=0 --enable-autoscaling --min-nodes=0 \
+    --max-nodes=${MAX_DASK_NODES} --node-taints ${dask_taints} \
+    --node-labels ${dask_labels} ${PREEMPTIBLE_FLAG}
+
+# make sure you have the credentials for this cluster loaded
+echo; echo get credentials for cluster
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE \
+  --project $PROJECTID
+
+# #this enables autoscaling of the cluster
+# gcloud container node-pools create worker-pool --zone=$ZONE --cluster=$CLUSTER_NAME --machine-type=n1-standard-8 --preemptible --enable-autoscaling --num-nodes=$MIN_WORKER_NODES --max-nodes=$MAX_WORKER_NODES --min-nodes=$MIN_WORKER_NODES --disk-size=50
 
 #this will give you admin access on the cluster
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
 
-#we create a service account tiller in the kube-system namespace
-#tiller is the server/cluster side tool for helm to install and manage our containers
-#This produce a service account names tiller in the kube cluster
-kubectl --namespace kube-system create sa tiller
+echo; echo creating namespace...
+kubectl create namespace $DEPLOYMENT_NAME
 
-#Give the tiller process cluster-admin status
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-
-#strangely this allows helm to install tiller into the kubernetes cluster
-helm init --service-account tiller
-
-# this patches the security of the deployment so that no other processes in the cluster can access the other pods
-kubectl --namespace=kube-system patch deployment tiller-deploy --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
-
-# Make sure you are in the rhg-hub repo
-# update the jupyterhub dependency just to check
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
-helm dependency update rhg-hub
+# Make sure you are in the rhg-hub repo for this:
+echo; echo add pangeo repo to cluster...
+helm repo add pangeo https://pangeo-data.github.io/helm-chart/
+helm repo update
 
 #Install the dask images from helm
 ## --name and --namespace are arbitary values I use to identify this deployment##
-helm install rhg-hub --name={release} --namespace={deployment_name} -f rhg-hub/values.yml -f rhg-hub/secret.yaml
+# helm install rhg-hub --name={release} --namespace={deployment_name} -f rhg-hub/values.yml -f rhg-hub/secret.yaml
